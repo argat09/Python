2faea4c9961c632f7d58831b5837e2466c15e699
argat09 <argat09@mail.ru>
2020-09-09 21:43:20 +0300

    END

--- Lab_01/01_lesson_01.py
+++ Lab_01/01_lesson_01.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
-x=[1,2,3,4,5,6,7,8,9,10]
+x=[1,10,1000]
+
+a = np.e**(1/(np.sin(x)+1))
+
+for i in x:
+    print (i)
+    b = 5/4 +(i**1)*15
+    y = (np.log(a/b)) / (np.log(1+i**2))
 
 print (x)
+
+print (y)
 
